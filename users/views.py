ba18e55b7fd72020f5bccecf2ffc64377bb67b21
Cpicon <c.picon@uniandes.edu.co>
2020-11-05 20:35:31 -0500

    Sign up view implemented.

--- users/views.py
+++ users/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.db.utils import IntegrityError
 
+#models
+from django.contrib.auth.models import User
+User._meta.get_field('email')._unique = True
+from users.models import Profile
 
 # Create your views here.
 
@@ -24,3 +29,25 @@ def logout_view(request):
     """Logout a user"""
     logout(request)
     return redirect('login')
+
+def singup_view(request):
+    """Sign up a user"""
+    if request.method =='POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        password_confirm = request.POST['password_confirmation']
+        if password != password_confirm:
+            return render(request, 'users/singup.html', {'error': ' Password confirmation does not match'})
+        else:
+            try:
+                user = User.objects.create_user(username, password)
+            except IntegrityError:
+                return render(request, 'users/singup.html', {'error': 'Username already exists'})
+            user.first_name = request.POST['first_name']
+            user.last_name = request.POST['last_name ']
+            user.email = request.POST['email']
+            profile = Profile.objects.create(user)
+            profile.save()
+            user.save()
+        return redirect('login')
+    return render(request, 'users/signup.html')
